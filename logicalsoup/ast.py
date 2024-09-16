c822c39b13b8560e308bc06ded9f52974be8165d
capricorn <8250820+capricorn@users.noreply.github.com>
2024-09-16 11:42:23 -0600

    First attempt at tree rewriting predicate

--- logicalsoup/ast.py
+++ logicalsoup/ast.py
@@ -20,6 +20,79 @@ def build_logicalsoup_predicates():
             ; (Node = element(_, Children), match(Children, Expr)).
         match(Expr) :-
             match({prolog_ast}, Expr).
+
+        % Test: wrap every (individual) node in 'hello(Node)'
+        hello(Expr).
+        hello([], SiblingExpr) :-
+            SiblingExpr = [].
+        hello([Child|Siblings], ResultExpr) :-
+            hello(Siblings, SiblingExpr),
+            append([hello(Child)], SiblingExpr, ResultExpr).
+
+        % An example: visiting a tree and wrapping each element in 'hello' 
+        helloVisit([], ResultExpr) :-
+            ResultExpr = [].
+        % This performs the traversal
+        helloVisit([Child|Siblings], ResultExpr) :-
+            helloVisit(Child, ChildExpr),
+            helloVisit(Siblings, SiblingExpr),
+            append([ChildExpr], [SiblingExpr], ResultExpr).
+        % This performs the map
+        helloVisit(Expr, ResultExpr) :- 
+            Expr = element(Tag, Children),
+            helloVisit(Children, ChildExpr),
+            % ResultExpr = hello(element(Tag, ChildExpr)).
+            ResultExpr = element(Tag, ChildExpr).
+
+        % Next change: use match to see if an element should be substituted
+        % Then, provide AST as arg
+        % Either: have a match (and a rewrite) or visit the next element
+        % Consider: Rewrite how match works?
+
+        % Test this on a few cases
+        % NB. Expr may be some deeply nested thing; 
+        % helloMatch(Expr, MatchExpr) :-
+        %    (Expr = MatchExpr)
+        %    ; (Expr = element(Tag, Children), )
+
+        helloVisit([], MatchExpr, RewriteExpr, ResultExpr) :-
+            ResultExpr = [].
+
+        % This performs the traversal
+        helloVisit([Child|Siblings], MatchExpr, RewriteExpr, ResultExpr) :-
+            helloVisit(Child, MatchExpr, RewriteExpr, ChildExpr),
+            helloVisit(Siblings, MatchExpr, RewriteExpr, SiblingExpr),
+            append([ChildExpr], [SiblingExpr], ResultExpr).
+
+        % Consider: Treat MatchExpr and RewriteExpr as predicates, call them?
+
+        helloVisit(Expr, MatchExpr, RewriteExpr, ResultExpr) :-
+            % (match(Expr, MatchExpr), helloVisit(RewriteExpr, ResultExpr))
+            % TODO: Is it a problem of this sort of match?
+            % (Expr = MatchExpr, customRewrite(Expr, NewExpr), helloVisit(NewExpr, MatchExpr, RewriteExpr, ResultExpr))
+            (customRewrite(Expr, NewExpr), helloVisit(NewExpr, MatchExpr, RewriteExpr, ResultExpr), !)
+            ; (Expr = element(Tag, Children), helloVisit(Children, MatchExpr, RewriteExpr, ChildrenExpr), ResultExpr = element(Tag, ChildrenExpr)).
+        
+        
+        visit(Rewriter, [], ResultExpr) :-
+            ResultExpr = [].
+        visit(Rewriter, [Child|Siblings], ResultExpr) :-
+            visit(Rewriter, Child, ChildExpr),
+            visit(Rewriter, Siblings, SiblingExpr),
+            append([ChildExpr], [SiblingExpr], ResultExpr).
+        visit(Rewriter, Expr, ResultExpr) :-
+            (rewrite(Rewriter, Expr, NewExpr), visit(Rewriter, NewExpr, ResultExpr), !)
+            ; (Expr = element(Tag, Children), visit(Rewriter, Children, ChildrenExpr), ResultExpr = element(Tag, ChildrenExpr)).
+    
+        % Provide a dynamic predicate such as:
+        customRewrite(Expr, NewExpr) :-
+            Expr = element(div, Children),
+            NewExpr = element(p, Children).
+        
+        % Example of applying a rule via call -- make a 'helloVisit' declaration that is wrapped with this
+        rewrite(Rule, Expr, NewExpr) :-
+            call(Rule, Expr, NewExpr).
+
         '''
     
     return '\n'.join([match_predicate]) 
