81e1f575a36da0ed0cfd3afad79a89d1084fe50e
capricorn <8250820+capricorn@users.noreply.github.com>
2024-09-17 10:30:08 -0600

    Load prolog AST predicates from jinja template

--- logicalsoup/ast.py
+++ logicalsoup/ast.py
@@ -1,5 +1,7 @@
-from bs4 import BeautifulSoup, Tag
 import argparse
+
+from bs4 import BeautifulSoup, Tag
+from jinja2 import Template
 
 def generate_prolog_ast(root_node):
     return generate_prolog_ast_rec(root_node)# + '.'
@@ -9,72 +11,11 @@ def generate_prolog_ast_rec(root_node):
     children_asts_str = f'[{', '.join(children_asts)}]'
     return f'element(\'{root_node.name}\', {children_asts_str})'
 
-def build_logicalsoup_predicates():
-    match_predicate = \
-        '''
-        match([Child|Siblings], Expr) :-
-            match(Child, Expr)
-            ; match(Siblings, Expr).
-        match(Node, Expr) :-
-            Node = Expr
-            ; (Node = element(_, Children), match(Children, Expr)).
-        match(Expr) :-
-            match({prolog_ast}, Expr).
-
-        visit(Rewriter, [], ResultExpr) :-
-            ResultExpr = [].
-        visit(Rewriter, [Child|Siblings], ResultExpr) :-
-            visit(Rewriter, Child, ChildExpr),
-            visit(Rewriter, Siblings, SiblingExpr),
-            append([ChildExpr], [SiblingExpr], ResultExpr).
-        visit(Rewriter, Expr, ResultExpr) :-
-            (rewrite(Rewriter, Expr, NewExpr), visit(Rewriter, NewExpr, ResultExpr), !)
-            ; (Expr = element(Tag, Children), visit(Rewriter, Children, ChildrenExpr), ResultExpr = element(Tag, ChildrenExpr)).
-
-        visitAST(Rewriter, ResultExpr) :-
-            visit(Rewriter, {prolog_ast}, ResultExpr).
+def build_logicalsoup_predicates(prolog_ast):
+    with open('ast.jinja') as f:
+        template = Template(f.read())
     
-        % Provide a dynamic predicate such as:
-        customRewrite(Expr, NewExpr) :-
-            Expr = element(div, Children),
-            NewExpr = element(p, Children).
-        
-        % Example of applying a rule via call -- make a 'helloVisit' declaration that is wrapped with this
-        rewrite(Rule, Expr, NewExpr) :-
-            call(Rule, Expr, NewExpr).
-
-        % Need to specify what the possibilities are
-        % TODO: Generate this from document? Other options?
-        % (Obviously can declare as facts, etc)
-        tag(Tag) :-
-            Tag = 'div' 
-            ; Tag = 'span'
-            ; Tag = '[document]'
-            ; Tag = 'td'
-            ; Tag = 'tr'
-            ; Tag = 'center'.
-
-        % HTML AST
-        element(Tag, []) :-
-            tag(Tag).
-        element(Tag, [Child|Siblings]) :-
-            tag(Tag),
-            Child = element(ChildTag, ChildChildren),
-            element(ChildTag, ChildChildren),
-            element(Tag, Siblings).
-
-        formatElement([], '').
-        formatElement([E|Es], Output) :-
-            formatElement(E, EOut),
-            formatElement(Es, EsOut),
-            format(atom(Output), '~a~a', [EOut, EsOut]).
-        formatElement(Element, Output) :-
-            Element = element(Tag, Children),
-            formatElement(Children, ChildOut),
-            format(atom(Output), '<~a>~a</~a>', [Tag, ChildOut, Tag]).
-        '''
-    
-    return '\n'.join([match_predicate]) 
+    return template.render(prolog_ast=prolog_ast)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -86,4 +27,4 @@ if __name__ == '__main__':
     
     soup = BeautifulSoup(html, 'html.parser')
     prolog_ast = generate_prolog_ast(soup)
-    print(build_logicalsoup_predicates().format(prolog_ast=prolog_ast))
+    print(build_logicalsoup_predicates(prolog_ast=prolog_ast))
